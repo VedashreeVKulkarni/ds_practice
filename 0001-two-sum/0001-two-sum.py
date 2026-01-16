9bf385c5804ae7898456e55b0b364396f598dfde
VEDASHREE V KULKARNI <vedashree369@gmail.com>
2026-01-16 20:10:06 +0530

    Update 0001-two-sum.py

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,9 +5,5 @@ class Solution:
                 if nums[i] + nums[j] == target:
                     return [i, j]
 
-                    #veda
-nums = [2,7,11,15]
-target = 9
-output = Solution().twoSum(nums, target)  
-print(output)  # Output: [0, 1]
+
 
