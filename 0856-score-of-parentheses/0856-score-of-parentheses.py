6a3dc7b479a79295d600e4c92eb731607e09c7e4
VEDASHREE V KULKARNI <vedashree369@gmail.com>
2026-09-23 23:31:56 +0530

    Time: 0 ms (100%), Space: 19.3 MB (55.47%) - LeetHub

---
+++ 0856-score-of-parentheses/0856-score-of-parentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def scoreOfParentheses(self, s: str) -> int:
+        score=0
+        stack=[0]
+        for x in s:
+         if x=='(':
+            stack.append(0)
+         else:    
+            A=stack.pop()
+            if A==0:
+                score=1
+            else:
+                score=2*A
+            stack[-1]+=score    
+        return stack[0]          
+        
